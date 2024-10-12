d538a315f9d508d4270aec6eccc9b9a4f92997bc
Natesh055 <singhnatesh50@gmail.com>
2024-10-12 08:42:18 +0530

    Minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import { useState, useCallback,useEffect } from 'react'
+import { useState, useCallback,useEffect,useRef } from 'react'
 import './App.css'
 
 function App() {
   const [length, setLength] = useState(8)
   const [numberAllowed, setNumberAllowed] = useState(false)
   const [charAllowed, setCharAllowed] = useState(false)
+
+
   const [password, setPassword] = useState("")
+
+
+
+
+
+    const passref = useRef(null)
+
+
+
+
+
+
+   // Dependencies: You provide a dependency array as the
+   //  second argument. The function will only be recreated 
+  // if one of the dependencies changes.
 
   const passwordgenerator = useCallback(() => {
     let pass = "";
@@ -21,18 +38,33 @@ function App() {
     setPassword(pass);
   }, [length, numberAllowed, charAllowed]);
 
-  const copyToClipboard = () => {
-    navigator.clipboard.writeText(password);
-    alert("Password copied to clipboard!");
-  };
+  // The array [length, numberAllowed, charAllowed] lists the dependencies 
+  // for the passwordgenerator function. This means that the function will
+  //  be re-created whenever any of these values change
+
+
+
+  // By using useCallback, the function is memoized, 
+  // meaning that it will not be recreated on every render unless one of the dependencies changes
 
 
 
 
 
-  useEffect(()=>{
-    passwordgenerator()
-  },[length, numberAllowed, charAllowed,passwordgenerator])
+  const copyToClipboard = useCallback(()=>
+  {
+    window.navigator.clipboard.writeText(password)
+    alert("Copied to clipboard")
+  },
+  [password])
+
+
+
+
+
+  // useEffect(()=>{
+  //   passwordgenerator()
+  // },[length, numberAllowed, charAllowed,passwordgenerator])
 
 
 
@@ -97,7 +129,7 @@ function App() {
 
       </div>
         
-        {/* <button className='button-34' onClick={passwordgenerator}>Generate Password</button> */}
+        <button className='button-34' onClick={passwordgenerator}>Generate Password</button>
       </div>
     </>
   );
